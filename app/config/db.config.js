4b0e0fd18b319f4aac5c06f1574cdec795232fbf
BuildTools <unconfigured@null.spigotmc.org>
2023-11-19 23:16:12 +0700

    5 commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,10 +4,10 @@ module.exports = {
   // PASSWORD: "",
   // DB: "webhw5",
 
-  HOST: "aws.connect.psdb.cloud",
-  USER: "9u9jz1iwvydn1is6nw9v",
-  PASSWORD: "pscale_pw_8rQhFwuFCZz1MLu8JLhHW9bOGqQy9B56itzzmDC9EKK",
-  DB: "backenddb",
+  // HOST: "aws.connect.psdb.cloud",
+  // USER: "9u9jz1iwvydn1is6nw9v",
+  // PASSWORD: "pscale_pw_8rQhFwuFCZz1MLu8JLhHW9bOGqQy9B56itzzmDC9EKK",
+  // DB: "backenddb",
 
   // HOST: "sql.freedb.tech",
   // USER: "freedb_Pongsakorn",
