1933bd5daaeb4cbc6285445fe078896d05a269db
BuildTools <unconfigured@null.spigotmc.org>
2023-11-19 20:32:57 +0700

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // HOST: "localhost",
+  // USER: "root",
+  // PASSWORD: "",
+  // DB: "webhw5",
+
+  HOST: "sql.freedb.tech",
+  USER: "freedb_Pongsakorn",
+  PASSWORD: "57t@PWn!jxQ5h*#",
+  DB: "freedb_webhw5",
+  
+};
+
