f07d78526524b5bfe6516720e142187cd60a0bf4
BuildTools <unconfigured@null.spigotmc.org>
2023-11-19 23:09:12 +0700

    4 commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,10 +4,14 @@ module.exports = {
   // PASSWORD: "",
   // DB: "webhw5",
 
-  HOST: "sql.freedb.tech",
-  USER: "freedb_Pongsakorn",
-  PASSWORD: "qSgFY9EK&dy9TgQ",
-  DB: "freedb_BackendDB",
+  HOST: "aws.connect.psdb.cloud",
+  USER: "9u9jz1iwvydn1is6nw9v",
+  PASSWORD: "pscale_pw_8rQhFwuFCZz1MLu8JLhHW9bOGqQy9B56itzzmDC9EKK",
+  DB: "backenddb",
 
+  // HOST: "sql.freedb.tech",
+  // USER: "freedb_Pongsakorn",
+  // PASSWORD: "qSgFY9EK&dy9TgQ",
+  // DB: "freedb_BackendDB",
 };
 
--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2");
-const dbConfig = require("../config/db.config");
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-});
+require('dotenv').config();
+// const dbConfig = require("../config/db.config");
+// const connection = mysql.createConnection({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB,
+// });
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
 connection.connect((error) => {
   if (error) console.log("mysql conection " + error);
   else console.log("Successfully conected to the database");
